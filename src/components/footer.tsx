0a1862004246bba539f4b70a30ab0e1bba591f3d
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-06-20 11:00:28 -0700

    updated footer info

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,8 @@ export interface FooterProps {
 export class Footer extends React.Component<FooterProps> {
     render() {
         // TODO update footer for page when non-hosted in the MakeCode editor
-        return <div className="ui segment footer">
+        return <div className="ui center basic segment footer">
+            <hr/>
             TODO: Update copyright / privacy notice.
         </div>
     }
